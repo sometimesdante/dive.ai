262c6b8785b0e89b5789733a1ba702f19079fb45
sometimesdante <lalithk3991@gmail.com>
2026-04-05 19:02:18 +0530

    server bugfix
    Type error: File '/vercel/path0/src/app/(frontend)/about/page.tsx' is not a module.

---
+++ src/app/(frontend)/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div className="default-height flex items-center overflow-clip">
+      <div className="w-full">
+        <h1 className="font-semibold">About</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(frontend)/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+  return (
+    <div className="default-height flex items-center overflow-clip">
+      <div className="w-full">
+        <h1 className="font-semibold">Contact</h1>
+      </div>
+    </div>
+  );
+}
